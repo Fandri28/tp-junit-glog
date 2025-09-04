bb6479b971a89c9142160bbdf98eb1a95bc954f7
milanfandresena <milanfandresena28@gmail.com>
2025-09-04 11:11:38 +0300

    question 4 5 6

--- src/main/java/fr/emse/test/Money.java
+++ src/main/java/fr/emse/test/Money.java
@@ -1,5 +1,7 @@
 package fr.emse.test;
 
+import java.util.Objects;
+
 class Money {
     private int fAmount;
     private String fCurrency;
@@ -18,6 +20,19 @@ class Money {
     }
 
     public Money add(Money m) {
+        if (!currency().equals(m.currency())) {
+            throw new IllegalArgumentException("Devises différentes !");
+        }
         return new Money(amount() + m.amount(), currency());
     }
+  
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true; // même référence
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Money money = (Money) obj;
+        return fAmount == money.fAmount && fCurrency.equals(money.fCurrency);
+    }
+
+    
 }
